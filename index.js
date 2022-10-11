3c5258fe6d02aa251b45018115f927f4eadedf69
Zoe Guidry <oyezoey@gmail.com>
2022-10-11 12:02:49 -0500

    commit tree a001eeeed8668140af1b8e64e64dcb83abf7b183

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceFt){
+  if (distanceFt <= 400) {
+    return 'This one is on me!';
+  } else if (distanceFt <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distanceFt <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipQuality){
+  switch (tipQuality) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+
+  }
 }
